05add241b7ea5bb0f59760caa87a5725ea972f3d
and-lim <andrianlimnardy@gmail.com>
2023-02-10 08:37:48 +0700

    first commit

---
+++ app/src/main/java/com/com/eazynote/MainActivity.java
@@ -0,0 +1,16 @@
+package com.com.eazynote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notepad);
+    }
+
+
+}
